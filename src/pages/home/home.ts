ef8c6fba4f4a809cdb0b5662b44d4c9a7621b31e
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2019-04-20 01:53:52 +0300

    add routing to second page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SecondPage } from '../second/second';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController) {
 
+  }
+
+  navigateToSecondPage(): void {
+    this.navCtrl.push(SecondPage);
   }
 
 }
